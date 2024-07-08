7a131c15d635a0f2685e530d130e5cfb03cefdd2
KiddMiguel <kidimbamiguel@gmail.com>
2024-07-08 22:12:12 +0200

    khg

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+const mysql = require('mysql');
+const config = require('config');
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/archivagePro');
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(`Error: ${err.message}`);
-        process.exit(1);
-    }
-};
+const db = mysql.createConnection({
+  host: config.get('mysqlHost'),
+  user: config.get('mysqlUser'),
+  password: config.get('mysqlPassword'),
+  database: config.get('mysqlDatabase')
+});
 
-module.exports = connectDB;
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL Connected...');
+});
+
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,12 @@
-require('dotenv').config();
 const express = require('express');
-const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const db = require('./config/db');
+
 const app = express();
 
-connectDB();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
-app.use(helmet());
-app.use(morgan('common'));
-
-app.use(express.json());
-
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 5000}`);
-});
+const port = process.env.PORT || 5001;
+app.listen(port, () => console.log(`Server started on port ${port}`));
