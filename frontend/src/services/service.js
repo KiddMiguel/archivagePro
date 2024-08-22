3b5eec3d949693754732b4551a5c14b059638549
KiddMiguel <kidimbamiguel@gmail.com>
2024-08-23 00:00:58 +0200

    Ajuster

--- frontend/src/services/service.js
+++ frontend/src/services/service.js
@@ -232,7 +232,7 @@ export const downloadFile = async (fileId) => {
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.setAttribute('download', fileName); 
+    link.setAttribute('download', decodeURIComponent(escape(fileName))); 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
